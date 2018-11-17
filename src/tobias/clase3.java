df3ec598a2818dd6b867af90848d6b94bd3a8721
Tobias Wild <tobias.wild2704@gmail.com>
2018-11-17 21:43:59 +0100

    tobi the man

--- src/tobias/clase3.java
+++ src/tobias/clase3.java
@@ -5,6 +5,8 @@ public class clase3 {
 	public static void main(String[] args) {
 	System.out.println("tobi");
 	System.out.println("tobi");
+	System.out.println("tobi");	System.out.println("tobi");	System.out.println("tobi");	System.out.println("tobi");	System.out.println("tobi");
+	
 	}
 
 }
