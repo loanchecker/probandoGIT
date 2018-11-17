0bb9a1a7cb02fbf73487e58ce2b2e21e9f10d9d9
juanp <juanp@LAPTOP-B159B7ON>
2018-11-17 17:48:21 -0300

    clases 3

--- src/juan/Clase3.java
+++ src/juan/Clase3.java
@@ -3,7 +3,8 @@ package juan;
 public class Clase3 {
 	public void main(String[] args) {
 		System.out.println("uraaaa");
-
+		System.out.println("uraaaa");
+		System.out.println("uraaaa");
 	}
 		
 }
